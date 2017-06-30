008e0f749584d5c4d1473e21e10a0ec7f9fef3ea
Juan E. Tenorio Arzola <tenoriojuann@gmail.com>
2017-06-30 00:38:40 -0400

    Login methods work and also creating users works

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import {_getAngularFireAuth, AngularFireAuth} from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
-
+import {FormControl, FormGroup} from "@angular/forms";
+import {Router} from "@angular/router";
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -12,22 +13,52 @@ export class LoginComponent implements OnInit {
 
   user: Observable<firebase.User>;
 
-  constructor(public afAuth: AngularFireAuth) {
+  myform: FormGroup;
+
+
+  ngOnInit() {
+
+    this.myform = new FormGroup({
+      name: new FormGroup({
+        firstName: new FormControl(),
+        lastName: new FormControl(),
+      }),
+      email: new FormControl(),
+      password: new FormControl(),
+      language: new FormControl()
+    });
+  }
+  constructor(public afAuth: AngularFireAuth, private router: Router) {
     this.user = afAuth.authState;
   }
 
 
-  login() {
+  createUser(email, password) {
+
+    firebase.auth().createUserWithEmailAndPassword(email, password);
+  }
+
+  loginEmail(email: string, password: string ) {
 
 
-    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider()).then( function (result){
+    var route = this.router;
+    this.afAuth.auth.signInWithEmailAndPassword(email, password).then(function (result){
+      console.log(result);
+      route.navigate(['']);
+      this.r
+    }).catch( function (error){
+      console.log(error);
+    });
 
-      window.location.href = "/";
+  }
 
-    }).
-      catch(function (error) {
-      console.log(error.message);
-    })
+  loginGoogle(){
+
+    var route = this.router;
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider).then( function (result) {
+      route.navigate(['']) ;
+      console.log(result);
+    });
   }
 
   logout() {
@@ -35,7 +66,5 @@ export class LoginComponent implements OnInit {
   }
 
 
-  ngOnInit() {
-  }
 
 }
