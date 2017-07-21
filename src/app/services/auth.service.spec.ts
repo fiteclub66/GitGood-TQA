995de133d9ec826ffe1ecebda0bf75b1015f860d
Juan E. Tenorio Arzola <tenoriojuann@gmail.com>
2017-07-20 23:33:47 -0400

    set up for the AuthService test

--- src/app/services/auth.service.spec.ts
+++ src/app/services/auth.service.spec.ts
@@ -1,15 +1,20 @@
 import { TestBed, inject } from '@angular/core/testing';
-
+import * as firebase from 'firebase/app';
 import { AuthService } from './auth.service';
+import {AngularFireAuth} from "angularfire2/auth";
+import {AngularFireModule, FirebaseApp, FirebaseAppName} from "angularfire2";
+import {firebaseConfig} from "../../environments/environment.prod";
+import {AngularFireDatabase} from "angularfire2/database";
 
 describe('AuthService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [AuthService]
-    });
+      providers: [AuthService, AngularFireAuth, AngularFireDatabase],
+      imports:[AngularFireModule.initializeApp(firebaseConfig)]
+    }).compileComponents();
   });
 
-  it('should be created', inject([AuthService], (service: AuthService) => {
+  it('should be created', inject([AuthService, AngularFireAuth, FirebaseApp, AngularFireDatabase], (service: AuthService) => {
     expect(service).toBeTruthy();
   }));
 });
